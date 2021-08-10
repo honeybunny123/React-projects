f72479731c99e04283c6a7d2c86b150054a8577b
Anitha Krish <anithakrish@FULL633-PRO.local>
2021-08-10 07:33:22 +0530

    Revert "implement hoc using function"
    This reverts commit a3f38a76d07c6fb7fd93440dbddc6e7e485fe5fb.

--- concepts/hoc-using-func/src/ClickCounter.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import withCounter from "./withCounter";
-
-const ClickCounter = (props)=> {
-  const {count, incrementCount } = props;
-  return (<button onClick={incrementCount}>Clicked {count} times </button>) 
-}
-
-export default withCounter(ClickCounter, 5);
